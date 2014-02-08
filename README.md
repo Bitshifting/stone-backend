ad1a088846aee76e1fac0a1e25db5f896a659258
Alex Kersten <alex@dividebyxero.com>
2014-02-07 23:17:16 -0600

    Better API and require.js stuff in server

--- README.md
+++ README.md
@@ -7,21 +7,21 @@ Backend for the stone system, using node and express.js
 #api
 When it's live, hit `[redacted].alexkersten.com`, port 3333, with the following:
 
-## `/stone/api/get_local_metadata`
+## `/stoneapi/api/get_local_metadata`
 Returns a list of local message IDs, within a radius (100m) of the user, along with the message rating and position.
 
 Send a JSON like `{"lat": 69.0, "lon": 420.0}`
 
 Returns a JSON like `[{"messageID": 1, "rating": 5.0, "lat": 44.4, "lon": 22.2}, {"messageID": 2, "rating": 1.0, "lat": 44.4, "lon": 22.2}, {"messageID": 3, "rating": 3.2, "lat": 44.4, "lon": 22.2}]`
 
-## `/stone/get_message_content`
+## `/stoneapi/get_message_content`
 Returns the content of a message based on its ID.
 
 Send a JSON like `{"messageID": 1337}`
 
-Returns a JSON like `{"message": "Look out!"}`
+Returns a JSON like `{"message": "Look out!", "rating": 5.0, "lat": 44.4, "lon": 22.2}`
 
-## `/stone/post_message`
+## `/stoneapi/post_message`
 Posts a message.
 
 Send a JSON like `{"message": "Try Jumping", "lat": 69.0, "lon": 123.4}`
--- stoneapi/server.js
+++ stoneapi/server.js
@@ -1,13 +1,26 @@
-var http = require('http');
+/*var http = require('http');
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<a href="https://github.com/akersten/stone-backend">See usage.</a>\n');
                   }).listen(3333, '127.0.0.1');
 console.log("Try connecting.");
+*/
 
 var express = require('express');
 var app = express();
 
-app.get('/get_local_metadata', function(req, res) {
-    res.send([{}])
-}
+app.get('/stoneapi/get_local_metadata', function(req, res) {
+    res.send('[{"messageID": 1, "rating": 5.0, "lat": 44.4, "lon": 22.2}, {"messageID": 2, "rating": 1.0, "lat": 44.4, "lon": 22.2}, {"messageID": 3, "rating": 3.2, "lat": 44.4, "lon": 22.2}]');
+});
+
+
+app.get('/stoneapi/get_message_content', function(req, res) {
+    res.send('[{"messageID": 1, "rating": 5.0, "lat": 44.4, "lon": 22.2}, {"messageID": 2, "rating": 1.0, "lat": 44.4, "lon": 22.2}, {"messageID": 3, "rating": 3.2, "lat": 44.4, "lon": 22.2}]');
+});
+
+
+app.get('/stoneapi/get_local_metadata', function(req, res) {
+    res.send('[{"messageID": 1, "rating": 5.0, "lat": 44.4, "lon": 22.2}, {"messageID": 2, "rating": 1.0, "lat": 44.4, "lon": 22.2}, {"messageID": 3, "rating": 3.2, "lat": 44.4, "lon": 22.2}]');
+});
+        app.listen(3333);
+console.log("Listening on 3333");
